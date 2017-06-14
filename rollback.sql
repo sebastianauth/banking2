3c33a1043167ae38e3352595c3c4480a37d0009a
Sebastian Auth <sebastian.auth@gmail.com>
2017-06-14 23:22:50 +0200

    #3 Add Unit tests - Initial test

--- rollback.sql
+++ rollback.sql
@@ -2,13 +2,13 @@
 -- SQL to roll back currently unexecuted changes
 -- *********************************************************************
 -- Change Log: classpath:/db/changelog/db.changelog-master.xml
--- Ran at: 15/04/17 15:08
+-- Ran at: 14/06/17 23:20
 -- Against: banking_db_user@jdbc:postgresql://localhost:5432/banking_dev?currentSchema=banking
 -- Liquibase version: 3.5.3
 -- *********************************************************************
 
 -- Lock Database
-UPDATE banking.databasechangeloglock SET LOCKED = TRUE, LOCKEDBY = 'sebastian-pc (192.168.230.1)', LOCKGRANTED = '2017-04-15 15:08:00.504' WHERE ID = 1 AND LOCKED = FALSE;
+UPDATE banking.databasechangeloglock SET LOCKED = TRUE, LOCKEDBY = 'sebastian-pc (192.168.230.1)', LOCKGRANTED = '2017-06-14 23:20:44.080' WHERE ID = 1 AND LOCKED = FALSE;
 
 -- Release Database Lock
 UPDATE banking.databasechangeloglock SET LOCKED = FALSE, LOCKEDBY = NULL, LOCKGRANTED = NULL WHERE ID = 1;
