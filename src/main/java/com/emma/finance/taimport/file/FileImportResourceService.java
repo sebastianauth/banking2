ff3f3569cb8f3f9c590b68f6e71342b0f0e67fc5
Sebastian Auth <sebastian.auth@gmail.com>
2017-07-02 12:55:32 +0200

    Thymeleaf included & reorganize Angular and Bootstrap dependecies

--- src/main/java/com/emma/finance/taimport/file/FileImportResourceService.java
+++ src/main/java/com/emma/finance/taimport/file/FileImportResourceService.java
@@ -10,17 +10,17 @@ import org.springframework.stereotype.Service;
 @Service
 public class FileImportResourceService implements ImportResourceService {
 	
-	private final Path rootLocation;
-	
-	@Autowired
-    public FileImportResourceService() {
-        this.rootLocation = Paths.get(null);
-    }
-
-	@Override
-	public ClassPathResource getImportResource() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+//	private final Path rootLocation;
+//	
+//	@Autowired
+//    public FileImportResourceService() {
+//        this.rootLocation = Paths.get(null);
+//    }
+//
+//	@Override
+//	public ClassPathResource getImportResource() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
 
 }
